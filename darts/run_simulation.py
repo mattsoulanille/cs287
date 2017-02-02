348ee9028dd6daa523cffa7d2f52f4976c32f39e
Matthew Soulanille <matthew@soulanille.net>
2017-02-02 10:00:12 -0500

    Added python multithreading to run_simulation

--- darts/run_simulation.py
+++ darts/run_simulation.py
@@ -1,15 +1,32 @@
 import _darts
 import numpy as np
 import matplotlib.pyplot as plt
-
+from multiprocessing import Pool
+import time
 
 count = 500000
-
+t0 = time.time()
 accuracies = np.arange(0,50,0.5);
 #accuracies = range(50);
 
-points20 = zip(*[[a, _darts.darts(count, a, 20)] for a in accuracies])
-points1 = zip(*[[a, _darts.darts(count, a, 1)] for a in accuracies])
+def throw(accuracy, target):
+    return [accuracy, _darts.darts(count, accuracy, target)]
+
+def throw20(accuracy):
+    return throw(accuracy, 20);
+
+def throw1(accuracy):
+    return throw(accuracy, 1);
+
+
+p = Pool(10);
+points20 = zip(*p.map(throw20, accuracies))
+points1 = zip(*p.map(throw1, accuracies))
+
+#points20 = zip(*[[a, _darts.darts(count, a, 20)] for a in accuracies])
+#points1 = zip(*[[a, _darts.darts(count, a, 1)] for a in accuracies])
+t1 = time.time()
+print("Execution time: " + str(t1-t0))
 
 # print(points20)
 # print(points1)

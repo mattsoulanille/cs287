94b21dd783b021bc81bbd7a36b27030b5e322bf9
Matthew Soulanille <msoulanill@haverford.edu>
2017-03-23 09:47:16 -0400

    Fixed MPI nodes trying to communicate through bridge interface

--- matrix/run.sh
+++ matrix/run.sh
@@ -3,4 +3,4 @@
 #mpirun -hostfile QuaCS matrixMPI ./testfiles/e3.txt 3 1
 #mpirun -hostfile QuaCS matrixMPI ./testfiles/e9.txt 9 1
 #mpirun -hostfile QuaCS matrixMPI ./testfiles/Matrix4.txt 10000 0
-mpirun -np $1 -hostfile QuaCS matrixMPI ./testfiles/Matrix3.txt 1000 0
+mpirun -np $1 --mca btl_tcp_if_include enp2s0 -hostfile QuaCS matrixMPI ./testfiles/Matrix3.txt 1000 0
